e7642493207fc754d8042ab0dea9a23153230be3
Jan Pikl <pikl.jan@gmail.com>
2013-01-26 02:56:10 +0100

    Redesigned feature edit part figure.

--- cz.jpikl.yafmt.ui.editors.fm/src/cz/jpikl/yafmt/ui/editors/fm/figures/FeatureFigure.java
+++ cz.jpikl.yafmt.ui.editors.fm/src/cz/jpikl/yafmt/ui/editors/fm/figures/FeatureFigure.java
@@ -1,20 +1,46 @@
 package cz.jpikl.yafmt.ui.editors.fm.figures;
 
+import org.eclipse.draw2d.BorderLayout;
 import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.Label;
-import org.eclipse.draw2d.LineBorder;
+import org.eclipse.draw2d.RoundedRectangle;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Pattern;
+import org.eclipse.swt.widgets.Display;
 
-public class FeatureFigure extends Label {
+public class FeatureFigure extends RoundedRectangle {
 
+    private Label label = new Label();
+    
     public FeatureFigure(String featureName) {
-        super(featureName);
-        setBorder(new LineBorder());
-        setBackgroundColor(ColorConstants.white);
-        setOpaque(true);
+        label = new Label(featureName);
+        add(label);
+        setLayoutManager(new BorderLayout());
+        setConstraint(label, BorderLayout.CENTER);
+    }
+    
+    @Override
+    protected void fillShape(Graphics graphics) {
+        Rectangle bounds = getBounds();
+        Point top = bounds.getTop();
+        Point bottom = bounds.getBottom();
+        
+        Color lightBlue = new Color(Display.getCurrent(), 175, 201, 235);
+        Pattern pattern = new Pattern(Display.getCurrent(), top.x, top.y, bottom.x, bottom.y, lightBlue, ColorConstants.white);
+        
+        graphics.setBackgroundPattern(pattern);
+        super.fillShape(graphics);
+        graphics.setBackgroundPattern(null);
+        
+        pattern.dispose();
+        lightBlue.dispose();
     }
     
     public void setFeatureName(String name) {
-        setText(name);
+        label.setText(name);
     }
     
 }
