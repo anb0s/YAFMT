0c5f6eec1a638fd8bc96bb814c8546ded216e6f6
Jan Pikl <pikl.jan@gmail.com>
2013-01-27 04:47:13 +0100

    Changed decoration for connections.

--- cz.jpikl.yafmt.ui.editors.fm/src/cz/jpikl/yafmt/ui/editors/fm/figures/ConnectionFigure.java
+++ cz.jpikl.yafmt.ui.editors.fm/src/cz/jpikl/yafmt/ui/editors/fm/figures/ConnectionFigure.java
@@ -1,18 +1,64 @@
 package cz.jpikl.yafmt.ui.editors.fm.figures;
 
-import org.eclipse.draw2d.PolygonDecoration;
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.Ellipse;
 import org.eclipse.draw2d.PolylineConnection;
+import org.eclipse.draw2d.RotatableDecoration;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.PrecisionPoint;
 
 public class ConnectionFigure extends PolylineConnection {
     
-    public ConnectionFigure() {
-        PolygonDecoration decoration = new PolygonDecoration();
-        decoration.setTemplate(PolygonDecoration.TRIANGLE_TIP);
+    private CircleDecoration decoration;
+    
+    public ConnectionFigure(boolean mandatoryTarget) {
+        decoration = new CircleDecoration(mandatoryTarget);
         setSourceDecoration(decoration);
     }
     
     public void highlight(boolean value) {
         setLineWidth(value ? 2 : 1);
+    }
+    
+    public void setMandatoryTarget(boolean mandatoryTarget) {
+        decoration.setFilled(mandatoryTarget);
+    }
+    
+    private static class CircleDecoration extends Ellipse implements RotatableDecoration {
+    
+        private static final int SIZE = 10;
+        
+        public CircleDecoration(boolean filled) {
+            setForegroundColor(ColorConstants.black);
+            setFilled(filled);
+            setSize(SIZE, SIZE);
+        }
+        
+        public void setFilled(boolean filled) {
+            setBackgroundColor(filled ? ColorConstants.black : ColorConstants.white);
+        }
+        
+        @Override
+        public void setReferencePoint(Point ref) {
+            Point a = getLocation().getCopy();
+            Point ab = ref.getTranslated(a.getNegated());
+            int diameter = getSize().height;
+            PrecisionPoint center = new PrecisionPoint();
+  
+            center.setPreciseX(a.x + (ab.x * diameter) / (2. * Math.sqrt(ab.x * ab.x + ab.y * ab.y)) - diameter / 2.0);
+            center.setPreciseY(a.y + (ab.y * diameter) / (2. * Math.sqrt(ab.x * ab.x + ab.y * ab.y)) - diameter / 2.0);
+  
+            center.x = (int) Math.round(center.preciseX());
+            center.y = (int) Math.round(center.preciseY());
+  
+            if ((ab.x * diameter) / (2. * Math.sqrt(ab.x * ab.x + ab.y * ab.y)) < 0)
+                center.x++;
+            if ((ab.y * diameter) / (2. * Math.sqrt(ab.x * ab.x + ab.y * ab.y)) < 0)
+                center.y++;
+  
+            setLocation(center);
+        }
+    
     }
 
 }
--- cz.jpikl.yafmt.ui.editors.fm/src/cz/jpikl/yafmt/ui/editors/fm/parts/ConnectionEditPart.java
+++ cz.jpikl.yafmt.ui.editors.fm/src/cz/jpikl/yafmt/ui/editors/fm/parts/ConnectionEditPart.java
@@ -1,31 +1,63 @@
 package cz.jpikl.yafmt.ui.editors.fm.parts;
 
 import org.eclipse.draw2d.IFigure;
+import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.gef.EditPolicy;
 import org.eclipse.gef.editparts.AbstractConnectionEditPart;
 import org.eclipse.gef.editpolicies.ConnectionEndpointEditPolicy;
 
+import cz.jpikl.yafmt.model.fm.Feature;
+import cz.jpikl.yafmt.model.fm.FeatureModelPackage;
+import cz.jpikl.yafmt.model.util.IModelListener;
+import cz.jpikl.yafmt.model.util.ModelListenerAdapter;
 import cz.jpikl.yafmt.ui.editors.fm.figures.ConnectionFigure;
 import cz.jpikl.yafmt.ui.editors.fm.model.Connection;
 import cz.jpikl.yafmt.ui.editors.fm.policies.ConnectionEditPolicy;
 import cz.jpikl.yafmt.ui.editors.fm.policies.ConnectionSelectionPolicy;
 
-public class ConnectionEditPart extends AbstractConnectionEditPart {
+public class ConnectionEditPart extends AbstractConnectionEditPart implements IModelListener {
 
+    private Connection connection;
+    private ModelListenerAdapter listenerAdapter;
+    
     public ConnectionEditPart(Connection connection) {
+        this.connection = connection;
+        this.listenerAdapter = new ModelListenerAdapter(this);
         setModel(connection);
     }
     
     @Override
-    protected IFigure createFigure() {
-        return new ConnectionFigure();
+    public void activate() {
+        super.activate();
+        listenerAdapter.connect(connection.getTarget());
     }
-        
+    
+    @Override
+    public void deactivate() {
+        listenerAdapter.dispose();
+        super.deactivate();
+    }
+    
+    @Override
+    protected IFigure createFigure() {
+        boolean mandatoryTarget = connection.getTarget().isMandatory();
+        return new ConnectionFigure(mandatoryTarget);
+    }
+                
     @Override
     protected void createEditPolicies() {
         installEditPolicy(EditPolicy.SELECTION_FEEDBACK_ROLE, new ConnectionSelectionPolicy());
         installEditPolicy(EditPolicy.CONNECTION_ROLE, new ConnectionEditPolicy());
         installEditPolicy(EditPolicy.CONNECTION_ENDPOINTS_ROLE, new ConnectionEndpointEditPolicy());
+    }
+
+    @Override
+    public void modelChanged(Notification notification) {
+        if(notification.getFeatureID(Feature.class) == FeatureModelPackage.FEATURE__LOWER) {
+            boolean mandatoryTarget = connection.getTarget().isMandatory();
+            ((ConnectionFigure) getFigure()).setMandatoryTarget(mandatoryTarget);
+        }
+            
     }
 
 }
