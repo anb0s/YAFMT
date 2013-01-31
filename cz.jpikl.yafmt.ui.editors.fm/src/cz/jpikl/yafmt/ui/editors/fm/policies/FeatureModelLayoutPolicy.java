41f95413e5c2b78aa23c4439a176315601a7ec85
Jan Pikl <pikl.jan@gmail.com>
2013-01-31 11:18:09 +0100

    Moving group and feature together does not break layout.

--- cz.jpikl.yafmt.ui.editors.fm/src/cz/jpikl/yafmt/ui/editors/fm/policies/FeatureModelLayoutPolicy.java
+++ cz.jpikl.yafmt.ui.editors.fm/src/cz/jpikl/yafmt/ui/editors/fm/policies/FeatureModelLayoutPolicy.java
@@ -1,5 +1,9 @@
 package cz.jpikl.yafmt.ui.editors.fm.policies;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Rectangle;
@@ -30,6 +34,35 @@ public class FeatureModelLayoutPolicy extends XYLayoutEditPolicy {
     }
     
     @Override
+    protected Command getChangeConstraintCommand(ChangeBoundsRequest request) {
+        // Fix move command.
+        // Drop all group edit parts which parent feature is also being moved.
+        Dimension sizeDelta = request.getSizeDelta();
+        if((sizeDelta.width == 0) && (sizeDelta.height == 0)) {
+            // Find all feature edit parts which are also selected to move.
+            Set<Feature> features = new HashSet<Feature>();
+            for(Object editPart: getHost().getChildren()) {
+                if(((EditPart) editPart).getSelected() == EditPart.SELECTED_NONE)
+                    continue;
+                Object model = ((EditPart) editPart).getModel();
+                if(model instanceof Feature)
+                    features.add((Feature) model);
+            }
+            // Remove related group edit parts from request.
+            for(Iterator<?> it = request.getEditParts().iterator(); it.hasNext(); ) {
+                Object model = ((EditPart) it.next()).getModel();
+                if((model instanceof Group) && (features.contains(((Group) model).getParent())))
+                    it.remove();
+            }
+        }
+        // No need to proceed empty request.
+        if(request.getEditParts().isEmpty())
+            return null;
+        
+        return super.getChangeConstraintCommand(request);
+    }
+    
+    @Override
     protected Command createChangeConstraintCommand(ChangeBoundsRequest request, EditPart child, Object constraint) {
         LayoutData layoutData = ((FeatureModelEditPart) getHost()).getLayoutData();
         Object model = child.getModel();
@@ -46,5 +79,5 @@ public class FeatureModelLayoutPolicy extends XYLayoutEditPolicy {
 
         return null;
     }
-
+    
 }
