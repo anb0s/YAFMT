fb605557ee965f960436e034cd3cb348e04355fe
Jan Pikl <pikl.jan@gmail.com>
2013-01-31 17:41:31 +0100

    Command classes refactoring.

--- cz.jpikl.yafmt.ui.editors.fm/src/cz/jpikl/yafmt/ui/editors/fm/commands/SetFeatureNameCommand.java
+++ cz.jpikl.yafmt.ui.editors.fm/src/cz/jpikl/yafmt/ui/editors/fm/commands/SetFeatureNameCommand.java
@@ -2,12 +2,12 @@ package cz.jpikl.yafmt.ui.editors.fm.commands;
 
 import cz.jpikl.yafmt.model.fm.Feature;
 
-public class RenameFeatureCommand extends RecordingCommand {
+public class SetFeatureNameCommand extends RecordingCommand {
 
     private Feature feature;
     private String newName;
 
-    public RenameFeatureCommand(Feature feature, String newName) {
+    public SetFeatureNameCommand(Feature feature, String newName) {
         setLabel("Rename Feature " + feature.getName() + " to " + newName);
         this.feature = feature;
         this.newName = newName;
