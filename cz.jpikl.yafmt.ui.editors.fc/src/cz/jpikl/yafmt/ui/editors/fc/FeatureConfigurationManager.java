d575fe4c323698b7055f57e830b27206a10d3243
Jan Pikl <pikl.jan@gmail.com>
2013-03-06 19:10:06 +0100

    Updated feature configuration manager.

--- cz.jpikl.yafmt.ui.editors.fc/src/cz/jpikl/yafmt/ui/editors/fc/FeatureConfigurationManager.java
+++ cz.jpikl.yafmt.ui.editors.fc/src/cz/jpikl/yafmt/ui/editors/fc/FeatureConfigurationManager.java
@@ -1,18 +1,37 @@
 package cz.jpikl.yafmt.ui.editors.fc;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.emf.common.util.EList;
 
-import cz.jpikl.yafmt.model.fc.AttributeValue;
 import cz.jpikl.yafmt.model.fc.FeatureConfiguration;
 import cz.jpikl.yafmt.model.fc.Selection;
 import cz.jpikl.yafmt.model.fc.util.FeatureConfigurationUtil;
-import cz.jpikl.yafmt.model.fm.Attribute;
 import cz.jpikl.yafmt.model.fm.Feature;
+import cz.jpikl.yafmt.model.fm.Group;
 
 public class FeatureConfigurationManager {
-
+    
+    private static class VirtualSelection {
+        
+        private Selection selection;
+        private int insertPosition;
+        
+        public VirtualSelection(Selection selection, int insertPosition) {
+            this.selection = selection;
+            this.insertPosition = insertPosition;
+        }
+                
+    }
+    
+    // Parent to children virtual connections.
+    private Map<Selection, List<VirtualSelection>> virtualConnections = new HashMap<Selection, List<VirtualSelection>>();
+    // Children to parent virtual connections.
+    private Map<Selection, Selection> virtualConnectionsOpposite = new HashMap<Selection, Selection>();
+    
     private FeatureConfiguration featureConfig;
 
     public FeatureConfigurationManager(FeatureConfiguration featureConfig) {
@@ -21,26 +40,156 @@ public class FeatureConfigurationManager {
         rebuildVirtualConnections();
     }
     
+    // ===========================================================================
+    //  Repair operations
+    // ===========================================================================
+    
     private void repairFeatureConfiguration() {
         // Repair configuration structure against feature model copy.
-        repairFeatureConfiguration(featureConfig.getRoot());
+        Feature rootFeature = featureConfig.getFeatureModelCopy().getRoot();
+        Selection rootSelection = featureConfig.getRoot();
+        FeatureConfigurationUtil.repairSelection(rootFeature, rootSelection);
     }
     
-    private void repairFeatureConfiguration(Selection selection) {
-        // Repair configuration structure from given selection.
-        // Used when adding new selection
-    }
-    
-    
-    
-    
+    // ===========================================================================
+    //  Virtual connection related operations
+    // ===========================================================================
 
     private void rebuildVirtualConnections() {
-        // Visit all existing selections and build map of virtual connections to unselected features. 
+        virtualConnections.clear();
+        virtualConnectionsOpposite.clear();
+        
+        Feature rootFeature = featureConfig.getFeatureModelCopy().getRoot();
+        Selection rootSelection = featureConfig.getRoot();
+        createVirtualConnections(rootFeature, rootSelection);
+    }
+    
+    private void createVirtualConnections(Feature feature, Selection selection) {
+        // This expect that children selections are sorted in order given by feature configuration repair process. 
+        int startingIndex = 0;
+        startingIndex = createVirtualConnections(feature.getFeatures(), startingIndex, selection, Integer.MAX_VALUE);
+        
+        EList<Group> groups = feature.getGroups();
+        for(int i = 0; i < groups.size(); i++) {
+            Group group = groups.get(i);
+            startingIndex = createVirtualConnections(group.getFeatures(), startingIndex, selection, group.getUpper());
+        }
     }
 
-    public void contributeModelChildren(List<Object> modelChildren, Selection selection) {
-        // Add all child selections and unselected features (virtual connections) to modelChildren list.
+    private int createVirtualConnections(EList<Feature> childrenFeatures, int startingIndex, Selection parentSelection, int groupMax) {
+        // This expect that children selections are sorted in order given by feature configuration repair process.
+        EList<Selection> selections = parentSelection.getSelections();
+        
+        for(int i = 0; i < childrenFeatures.size(); i++) {
+            Feature childFeature = childrenFeatures.get(i);
+            String id = childFeature.getId();
+            int featureMax = childFeature.getUpper();
+                           
+            for(int j = startingIndex; j < selections.size(); j++) {
+                if(selections.get(j).getId().equals(id)) {
+                    featureMax--;
+                    groupMax--;
+                    startingIndex++;
+                }
+                else {
+                    break;
+                }
+            }
+            
+            if((groupMax > 0) && (featureMax > 0)) {
+                Selection childSelection = FeatureConfigurationUtil.createSelection(childFeature);
+                addVirtualConnection(parentSelection, childSelection, startingIndex);
+            }
+        }
+        
+        return startingIndex;
+    }
+
+    private void addVirtualConnection(Selection parentSelection, Selection childSelection, int position) {
+        // Parent to child connection.
+        List<VirtualSelection> childrenSelections = virtualConnections.get(parentSelection);
+        if(childrenSelections == null) {
+            childrenSelections = new ArrayList<VirtualSelection>();
+            virtualConnections.put(parentSelection, childrenSelections);
+        }
+        childrenSelections.add(new VirtualSelection(childSelection, position));
+        
+        // Child to parent connection.
+        virtualConnectionsOpposite.put(childSelection, parentSelection);
+    }
+        
+    // ===========================================================================
+    //  Query operations
+    // ===========================================================================
+    
+    public Selection getParentSelection(Selection childSelection) {
+        Selection parentSelection = childSelection.getParent();
+        if(parentSelection == null)
+            virtualConnectionsOpposite.get(childSelection);
+        return parentSelection;
+    }
+    
+    public List<Selection> getChildrenSelections(Selection parentSelection) {
+        List<Selection> childrenSelections = new ArrayList<Selection>();
+        contributeChildrenSelections(parentSelection, childrenSelections, true);
+        return childrenSelections;
+    }
+    
+    public void contributeChildrenSelections(Selection parentSelection, List<Selection> selectionList, boolean sortChildren) {
+        selectionList.addAll(parentSelection.getSelections());
+        
+        List<VirtualSelection> childrenSelections = virtualConnections.get(parentSelection);
+        if(childrenSelections == null)
+            return;
+        
+        if(sortChildren) {
+            int offset = 0;
+            for(int i = 0; i < childrenSelections.size(); i++) {
+                VirtualSelection childSelection = childrenSelections.get(i);
+                selectionList.add(childSelection.insertPosition + offset, childSelection.selection);
+                offset++;
+            }
+        }
+        else {
+            for(int i = 0; i < childrenSelections.size(); i++) {
+                VirtualSelection childSelection = childrenSelections.get(i);
+                selectionList.add(childSelection.selection);
+            }
+        }
+    }
+    
+    // ===========================================================================
+    //  Selections operations
+    // ===========================================================================
+        
+    public void selectFeatures(List<Selection> selections) {
+        for(Selection selection: selections)
+            selectFeature(selection);
+    }
+
+    public void selectFeature(Selection selection) {
+        Selection parentSelection = virtualConnectionsOpposite.get(selection);
+        if(parentSelection == null)
+            return;
+        
+        List<VirtualSelection> childrenSelections = virtualConnections.get(parentSelection);
+        if(childrenSelections == null)
+            return;
+        
+        int index = -1;
+        for(int i = 0; i < childrenSelections.size(); i++) {
+            if(childrenSelections.get(i).selection == selection) {
+                index = i;
+                break;
+            }
+        }
+        if(index == -1)
+            return;
+        
+        int insertPosition = childrenSelections.remove(index).insertPosition;
+        for(int i = index; i < childrenSelections.size(); i++)
+            childrenSelections.get(i).insertPosition++;
+        parentSelection.getSelections().add(insertPosition, selection);
     }
     
 }
