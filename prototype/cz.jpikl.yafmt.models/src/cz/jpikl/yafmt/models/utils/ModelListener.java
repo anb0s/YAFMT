1e9abb39004e7393b4ecbeb7d6eb6dbc26dab6db
Jan Pikl <pikl.jan@gmail.com>
2013-03-08 00:56:41 +0100

    Removed prototype code (it is already fully merged into the current plugin code).

--- prototype/cz.jpikl.yafmt.models/src/cz/jpikl/yafmt/models/utils/ModelListener.java
+++
@@ -1,9 +0,0 @@
-package cz.jpikl.yafmt.models.utils;
-
-import org.eclipse.emf.common.notify.Notification;
-
-public interface ModelListener {
-
-    public void modelChanged(Notification notification);
-
-}
