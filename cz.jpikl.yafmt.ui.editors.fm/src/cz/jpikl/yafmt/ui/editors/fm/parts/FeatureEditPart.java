842e381ed3bb8fd034119d64514c656ae4229e7f
Jan Pikl <pikl.jan@gmail.com>
2013-03-12 23:14:02 +0100

    Refactored feature edit part.

--- cz.jpikl.yafmt.ui.editors.fm/src/cz/jpikl/yafmt/ui/editors/fm/parts/FeatureEditPart.java
+++ cz.jpikl.yafmt.ui.editors.fm/src/cz/jpikl/yafmt/ui/editors/fm/parts/FeatureEditPart.java
@@ -1,5 +1,15 @@
 package cz.jpikl.yafmt.ui.editors.fm.parts;
 
+import static cz.jpikl.yafmt.model.fm.FeatureModelPackage.ATTRIBUTE__ID;
+import static cz.jpikl.yafmt.model.fm.FeatureModelPackage.FEATURE__ATTRIBUTES;
+import static cz.jpikl.yafmt.model.fm.FeatureModelPackage.FEATURE__FEATURES;
+import static cz.jpikl.yafmt.model.fm.FeatureModelPackage.FEATURE__GROUPS;
+import static cz.jpikl.yafmt.model.fm.FeatureModelPackage.FEATURE__LOWER;
+import static cz.jpikl.yafmt.model.fm.FeatureModelPackage.FEATURE__NAME;
+import static cz.jpikl.yafmt.model.fm.FeatureModelPackage.FEATURE__PARENT_FEATURE;
+import static cz.jpikl.yafmt.model.fm.FeatureModelPackage.FEATURE__PARENT_GROUP;
+import static cz.jpikl.yafmt.model.fm.FeatureModelPackage.FEATURE__UPPER;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,10 +19,12 @@ import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.common.notify.impl.AdapterImpl;
+import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EContentAdapter;
 import org.eclipse.gef.ConnectionEditPart;
+import org.eclipse.gef.EditPart;
 import org.eclipse.gef.EditPolicy;
 import org.eclipse.gef.NodeEditPart;
 import org.eclipse.gef.Request;
@@ -22,7 +34,6 @@ import org.eclipse.swt.SWT;
 import cz.jpikl.yafmt.model.fm.Attribute;
 import cz.jpikl.yafmt.model.fm.Feature;
 import cz.jpikl.yafmt.model.fm.FeatureModel;
-import cz.jpikl.yafmt.model.fm.FeatureModelPackage;
 import cz.jpikl.yafmt.model.validation.fm.FeatureModelValidator;
 import cz.jpikl.yafmt.ui.directediting.LabelDirectEditManager;
 import cz.jpikl.yafmt.ui.editors.fm.figures.FeatureFigure;
@@ -33,8 +44,6 @@ import cz.jpikl.yafmt.ui.editors.fm.policies.ConnectionCreationPolicy;
 import cz.jpikl.yafmt.ui.editors.fm.policies.FeatureDirectEditPolicy;
 import cz.jpikl.yafmt.ui.editors.fm.policies.FeatureEditPolicy;
 import cz.jpikl.yafmt.ui.editors.fm.policies.FeatureLayoutPolicy;
-import cz.jpikl.yafmt.ui.figures.ErrorDecoration;
-import cz.jpikl.yafmt.ui.figures.FigureDecorator;
 import cz.jpikl.yafmt.ui.util.NonEmptyCellEditorValidator;
 import cz.jpikl.yafmt.ui.validation.IProblemStore;
 
@@ -52,12 +61,18 @@ public class FeatureEditPart extends AbstractGraphicalEditPart implements NodeEd
         this.problemStore = problemStore;
         setModel(feature);
     }
+    
+    // ===================================================================
+    //  Initialization
+    // ===================================================================
 
     @Override
     public void activate() {
         super.activate();
         feature.eAdapters().add(featureAdapter);
+        revalidateModelRecursive();
         refreshLayoutData();
+        refreshVisuals();
     }
 
     @Override
@@ -66,76 +81,32 @@ public class FeatureEditPart extends AbstractGraphicalEditPart implements NodeEd
         super.deactivate();
     }
     
-    private void revalidateModel(boolean recursive) {
-        problemStore.clearProblems(feature);
-        if(recursive) {
-            for(Attribute attribute: feature.getAttributes())
-                problemStore.clearProblems(attribute);
-        }
-        BasicDiagnostic diagnostic = new BasicDiagnostic();
-        if(!FeatureModelValidator.INSTANCE.validate(feature, diagnostic))
-            problemStore.readProblems(diagnostic);
-        getErrorDecoration().setErrors(problemStore.getProblems(feature));
-    }
-
-    @Override
-    protected List<?> getModelChildren() {
-        return feature.getAttributes();
-    }
-
-    @Override
-    protected List<Object> getModelSourceConnections() {
-        EObject parent = feature.getParent();
-        if((parent == null) || (parent instanceof FeatureModel))
-            return null;
-
-        List<Object> connections = new ArrayList<Object>();
-        connections.add(new Connection(feature.getParent(), feature));
-        return connections;
-    }
-
-    @Override
-    protected List<Object> getModelTargetConnections() {
-        if(feature.getFeatures().isEmpty())
-            return null;
-
-        List<Object> connections = new ArrayList<Object>();
-        for(Feature child: feature.getFeatures())
-            connections.add(new Connection(feature, child));
-        return connections;
-    }
-
+    // ===================================================================
+    //  Visuals
+    // ===================================================================
+    
     @Override
     protected IFigure createFigure() {
-        FigureDecorator figure = new FigureDecorator(new FeatureFigure(feature));
-        figure.addDecoration(new ErrorDecoration(problemStore.getProblems(feature)));
-        return figure;
+        return new FeatureFigure(feature);
     }
 
     public FeatureFigure getFeatureFigure() {
-        return (FeatureFigure) ((FigureDecorator) getFigure()).getFigure();
-    }
-    
-    private ErrorDecoration getErrorDecoration() {
-        return (ErrorDecoration) ((FigureDecorator) getFigure()).getDecorations().get(0);
+        return (FeatureFigure) super.getFigure();
     }
     
     @Override
     protected void refreshVisuals() {
-        getFeatureFigure().refresh(); // Called when direct edit input is cancelled.
+        FeatureFigure figure = getFeatureFigure();
+        figure.setErrors(problemStore.getProblems(feature));
+        figure.refresh();
     }
-
-    public LayoutData getLayoutData() {
-        return layoutData;
+    
+    protected void refreshVisualsRecursive() {
+        refreshVisuals();
+        for(Object child: getChildren())
+            ((EditPart) child).refresh();
     }
-
-    private void refreshLayoutData() {
-        Rectangle bounds = layoutData.get(feature);
-        if(bounds == null)
-            bounds = new Rectangle(0, 0, FeatureFigure.INITIAL_WIDTH, FeatureFigure.INITIAL_HEGHT);
-        layoutData.set(feature, bounds);
-    }
-
+    
     @Override
     public ConnectionAnchor getSourceConnectionAnchor(ConnectionEditPart connection) {
         return new ShrinkedChopboxAnchor(getFigure(), 1);
@@ -155,6 +126,74 @@ public class FeatureEditPart extends AbstractGraphicalEditPart implements NodeEd
     public ConnectionAnchor getTargetConnectionAnchor(Request request) {
         return new ShrinkedChopboxAnchor(getFigure(), 1);
     }
+    
+    // ===================================================================
+    //  Layout
+    // ===================================================================
+    
+    public LayoutData getLayoutData() {
+        return layoutData;
+    }
+
+    private void refreshLayoutData() {
+        Rectangle bounds = layoutData.get(feature);
+        if(bounds == null)
+            bounds = createInitialLayoutData();
+        layoutData.set(feature, bounds);
+    }
+    
+    private Rectangle createInitialLayoutData() {
+        return new Rectangle(0, 0, FeatureFigure.INITIAL_WIDTH, FeatureFigure.INITIAL_HEGHT);
+    }
+    
+    // ===================================================================
+    //  Model
+    // ===================================================================
+    
+    private void revalidateModel() {
+        problemStore.clearProblems(feature);
+        BasicDiagnostic diagnostic = new BasicDiagnostic();
+        if(!FeatureModelValidator.INSTANCE.validate(feature, diagnostic))
+            problemStore.readProblems(diagnostic);
+    }
+    
+    private void revalidateModelRecursive() {
+        problemStore.clearProblems(feature);
+        for(Attribute attribute: feature.getAttributes())
+            problemStore.clearProblems(attribute);
+        BasicDiagnostic diagnostic = new BasicDiagnostic();
+        if(!FeatureModelValidator.INSTANCE.validateRecursive(feature, diagnostic))
+            problemStore.readProblems(diagnostic);
+    }
+
+    @Override
+    protected List<?> getModelChildren() {
+        return feature.getAttributes();
+    }
+
+    @Override
+    protected List<Object> getModelSourceConnections() {
+        EObject parent = feature.getParent();
+        if((parent == null) || (parent instanceof FeatureModel))
+            return null;
+        List<Object> connections = new ArrayList<Object>();
+        connections.add(new Connection(feature.getParent(), feature));
+        return connections;
+    }
+
+    @Override
+    protected List<Object> getModelTargetConnections() {
+        if(feature.getFeatures().isEmpty())
+            return null;
+        List<Object> connections = new ArrayList<Object>();
+        for(Feature child: feature.getFeatures())
+            connections.add(new Connection(feature, child));
+        return connections;
+    }
+
+    // ===================================================================
+    //  Policies and requests
+    // ===================================================================
 
     @Override
     protected void createEditPolicies() {
@@ -166,54 +205,72 @@ public class FeatureEditPart extends AbstractGraphicalEditPart implements NodeEd
 
     @Override
     public void performRequest(Request request) {
-        if(REQ_OPEN.equals(request.getType())) {
-            Label label = ((FeatureFigure) getFigure()).getLabel();
-            LabelDirectEditManager manager = new LabelDirectEditManager(this, label);
-            manager.setValidator(new NonEmptyCellEditorValidator());
-            manager.setAlignment(SWT.CENTER);
-            manager.show();
-        }
+        if(REQ_OPEN.equals(request.getType()))
+            performDirectEditing();
+    }
+
+    private void performDirectEditing() {
+        Label label = ((FeatureFigure) getFigure()).getLabel();
+        LabelDirectEditManager manager = new LabelDirectEditManager(this, label);
+        manager.setValidator(new NonEmptyCellEditorValidator());
+        manager.setAlignment(SWT.CENTER);
+        manager.show();
     }
     
-    private class FeatureAdapter extends AdapterImpl {
+    // ===================================================================
+    //  Events
+    // ===================================================================
+    
+    private class FeatureAdapter extends EContentAdapter {
 
         @Override
-        public void notifyChanged(Notification notification) {
+        protected void addAdapter(Notifier notifier) {
+            // Listen only to feature attributes.
+            if(notifier instanceof Attribute)
+                super.addAdapter(notifier);
+        }
+        
+        @Override
+        public void notifyChanged(Notification msg) {
+            if(msg.getNotifier() instanceof Feature)
+                notifyChangedFromFeature(msg);
+            else
+                notifyChangedFromAttribute(msg);            
+        }
 
-            switch(notification.getFeatureID(Feature.class)) {
-                case FeatureModelPackage.FEATURE__NAME:
-                case FeatureModelPackage.FEATURE__ID:
-                case FeatureModelPackage.FEATURE__DESCRIPTION:
-                    revalidateModel(false);
+        private void notifyChangedFromFeature(Notification msg) {
+            switch(msg.getFeatureID(Feature.class)) {
+                case FEATURE__NAME:
+                case FEATURE__LOWER:
+                case FEATURE__UPPER:
+                    revalidateModel();
                     refreshVisuals();
                     break;
-                    
-                case FeatureModelPackage.FEATURE__LOWER:
-                case FeatureModelPackage.FEATURE__UPPER:
-                    revalidateModel(false);
-                    break;
 
-                case FeatureModelPackage.FEATURE__PARENT_FEATURE:
-                case FeatureModelPackage.FEATURE__PARENT_GROUP:
+                case FEATURE__PARENT_FEATURE:
+                case FEATURE__PARENT_GROUP:
                     refreshSourceConnections();
                     break;
 
-                case FeatureModelPackage.FEATURE__FEATURES:
-                case FeatureModelPackage.FEATURE__GROUPS:
+                case FEATURE__FEATURES:
+                case FEATURE__GROUPS:
                     refreshTargetConnections();
                     break;
-
-                case FeatureModelPackage.FEATURE__ATTRIBUTES:
-                    switch(notification.getEventType()) {
-                        case Notification.ADD:
-                        case Notification.REMOVE:
-                        case Notification.MOVE:
-                            revalidateModel(true);
-                            refreshChildren();
-                            break;
-                    }
+                
+                case FEATURE__ATTRIBUTES:
+                    revalidateModelRecursive();
+                    refreshChildren();
+                    break;
             }
-            
+        }
+
+        private void notifyChangedFromAttribute(Notification msg) {
+            switch(msg.getFeatureID(Attribute.class)) {
+                case ATTRIBUTE__ID:
+                    revalidateModelRecursive();
+                    refreshVisualsRecursive();
+                    break;
+            }
         }
 
     }
